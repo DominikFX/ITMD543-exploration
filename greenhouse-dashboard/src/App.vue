c73de7ec6092da25df26f63f293e5fd0ac698dbc
Doxmin <mdaisonu@gmail.com>
2025-12-02 19:10:48 -0600

    case sensitive apparently

--- greenhouse-dashboard/src/App.vue
+++ greenhouse-dashboard/src/App.vue
@@ -11,6 +11,6 @@
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue';
+import Navbar from './components/NavBar.vue';
 import Footer from './components/Footer.vue';
 </script>
